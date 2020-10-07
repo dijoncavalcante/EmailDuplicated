be4cdba82ba4ef00f0bbbd8691f0101281e402f1
dijoncavalcante <dijoncavalcante@gmail.com>
2020-10-07 12:00:01 -0400

    Add Pessoa class with email atribute

---
+++ app/src/main/java/com/dijon/emailduplicated/Pessoa.java
@@ -0,0 +1,14 @@
+package com.dijon.emailduplicated;
+
+public class Pessoa {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
